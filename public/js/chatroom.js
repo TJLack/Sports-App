32aee2a8647540c0835fb3ca40f4175cd56c3597
RobertSimpson2619 <RobertSimpson2619@gmail.com>
2017-08-17 09:17:55 -0500

    added onclick scroll animation

---
+++ public/js/chatroom.js
@@ -0,0 +1,15 @@
+$("#infoBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#information").offset().top},
+        'slow');
+});
+$("#chatBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#chatroom").offset().top},
+        'slow');
+});
+$("#mapBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#map").offset().top},
+        'slow');
+});
