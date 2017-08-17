6ca803766613ed02d144bb9f6ead1cf46f877e22
RobertSimpson2619 <RobertSimpson2619@gmail.com>
2017-08-17 15:37:50 -0500

    added chat backend support

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,3 +1,5 @@
+//Page direction buttons
+
 $("#infoBtn").click(function() {
     $('html,body').animate({
         scrollTop: $("#information").offset().top},
@@ -13,3 +15,38 @@ $("#mapBtn").click(function() {
         scrollTop: $("#map").offset().top},
         'slow');
 });
+
+
+
+//chatroom submit clicked
+$("#chatSubmit").on("click",function(){
+
+	event.preventDefault();
+	console.log("New Chat message");
+
+	var userName = $("#username").val().trim();
+	var userMessage = $("#message").val().trim();
+	var groupName = $("#group").val().trim();
+
+	insertPost(userName,userMessage,groupName);
+})
+
+
+function insertPost(userName, userMessage, groupName) {
+    
+    var chat = {
+      title: userName,
+      body: userMessage,
+      group: groupName
+	};
+    $.post("/api/posts/", chat, checkPosts);
+}
+
+
+function checkPosts(){
+
+	//check all posts 
+
+	//append posts
+
+}
